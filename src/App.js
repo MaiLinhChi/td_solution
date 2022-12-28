e5c108631a521a217cc296205b171ffc442933ea
TIMOTHY TRAN <timothytnm@gmail.com>
2022-12-28 14:58:08 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import config from './config/index';
+import DefaultLayout from './layouts/DefaultLayout';
+import Home from './pages/Home';
+import Support from './pages/Support';
+import Career from './pages/Career';
+import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        path={config.routes.home}
+                        element={
+                            <DefaultLayout>
+                                <Home />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route
+                        path={config.routes.support}
+                        element={
+                            <DefaultLayout>
+                                <Support />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route
+                        path={config.routes.career}
+                        element={
+                            <DefaultLayout>
+                                <Career />
+                            </DefaultLayout>
+                        }
+                    />
+                    <Route
+                        path={config.routes.contact}
+                        element={
+                            <DefaultLayout>
+                                <Contact />
+                            </DefaultLayout>
+                        }
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
